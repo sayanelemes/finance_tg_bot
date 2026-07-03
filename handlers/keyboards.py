68208cb33fc3d46c032759d6d0eca7742823e7c7
sayanelemes <elemesaian12@gmail.com>
2026-07-03 23:26:33 +0500

    refactor: перевёл меню на инлайн-кнопки, убрал статус сервера и проверку админа

--- handlers/keyboards.py
+++ handlers/keyboards.py
@@ -1,11 +1,23 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton,
+                           InlineKeyboardButton, InlineKeyboardMarkup)
+                           
+from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
 
-def admin_keyboard():
-    rpls = ReplyKeyboardMarkup(
-        keyboard=[
-            [KeyboardButton(text="📊 СТАТУС СЕРВЕРА")]
-        ],   
-        resize_keyboard=True,
-        input_field_placeholder="Выберите действие"
-    )
-    return rpls
+
+
+main = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='📝 Добавить расход', callback_data='add_expence'),
+    InlineKeyboardButton(text='📊 Статистика', callback_data='view_stats'),
+    InlineKeyboardButton(text='Настройки', callback_data='settings')]
+    ])
+
+
+
+test = ['Профиль', 'Помощь', 'test']
+
+
+async def inline_test():
+    keyboard = InlineKeyboardBuilder()
+    for example in test:
+        keyboard.add(InlineKeyboardButton(text=example, url='https://github.com/sayanelemes'))
+    return keyboard.adjust(3).as_markup()
--- handlers/start.py
+++ handlers/start.py
@@ -1,69 +1,29 @@
-import psutil, os
+import handlers.keyboards as kb
 
-from dotenv import load_dotenv
+
 from aiogram import F, Router
 from aiogram.filters import CommandStart, Command
-from aiogram.types import Message
-from handlers.keyboards import admin_keyboard
+from aiogram.types import Message, CallbackQuery
+
 
 router = Router()
 
-load_dotenv()
-env_id = os.getenv('ADMIN_ID', '').strip()
-ADMIN_id = int(env_id) if env_id.isdigit() else None
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
-    await message.reply(f'Привет! твой ID: {message.from_user.id} Имя: {message.from_user.first_name}')
-
-
-@router.message(Command('commands'))  # Ловит команду /commands
-async def get_help(message: Message):
-    if ADMIN_id and message.from_user.id == ADMIN_id:
-        await message.answer(
-            "Вот доступные команды и меню:", 
-            reply_markup=admin_keyboard()
-        )
-
-
-
-
-@router.message(F.text == "📊 СТАТУС СЕРВЕРА")
-async def check_status(message: Message):
-    if ADMIN_id and message.from_user.id == ADMIN_id:
-        cpu_usage = psutil.cpu_percent(interval=None)
-        ram = psutil.virtual_memory()
-        disk = psutil.disk_usage('/')
-        battery = psutil.sensors_battery()
-
-    
-        ram_used = round(ram.used / (1024 ** 3), 2)
-        ram_total = round(ram.total / (1024 ** 3), 2)
-        disk_free = round(disk.free / (1024 ** 3), 2)
-        disk_total = round(disk.total / (1024 ** 3), 2)
-
-    
-        status_text = (
-            f"📊 СТАТУС СЕРВЕРА:\n\n"
-            f"🤖 Процессор: {cpu_usage}%\n"
-            f"🧠 ОЗУ: {ram_used} ГБ / {ram_total} ГБ ({ram.percent}%)\n"
-            f"💾 Свободно на диске: {disk_free} ГБ из {disk_total} ГБ\n"
-        )
-        if battery:
-            status_text += f"🔋 Батарея: {round(battery.percent)}%"
-            if battery.power_plugged:
-                status_text += f"\n⏳Осталось заряда на: ∞\n🔌Подключен к сети: Да"
-            else:
-                status_text += f"\n⏳Осталось заряда на: {(battery.secsleft // 60)//60} часов\n🔌 Подключен к сети: Нет"
-        else:
-            status_text += "🔋 Батарея: Не обнаружена\n"
+    await message.reply(f'Привет! {message.from_user.first_name}',
+        reply_markup=kb.main
+    )
+#@router.message(Command('commands'))  # Ловит команду /commands
+#async def get_help(message: Message):
+#    await message.answer(
+#        "Вот доступные команды и меню:"
+#        )
 
 
 
 
-    else:
-        status_text = "Нет доступа"
-        
-    await message.answer(status_text)
-
-
+@router.callback_query(F.data == 'settings')
+async def status(callback: CallbackQuery):
+    await callback.answer('Настройки')
+    await callback.message.edit_text('Настройки:', reply_markup=await kb.inline_test())
